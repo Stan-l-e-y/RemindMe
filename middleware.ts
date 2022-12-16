0f1d58cd538ba10ac64f2ce85278fc46e0dc3de5
Stanley Tsonev <stanleytsonev@hotmail.com>
2022-12-16 18:50:34 -0500

    middleware init

---
+++ middleware.ts
@@ -0,0 +1,6 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export default function middleware(req: NextRequest) {
+  return NextResponse.next();
+}
