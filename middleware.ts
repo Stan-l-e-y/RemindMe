0db0acc2e4f868d2b01ab6bcc76be8ee1bc573b0
Stanley Tsonev <stanleytsonev@hotmail.com>
2022-12-18 22:37:31 -0500

    downgrade to next12 for middleware conflict

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,38 @@
-import { NextResponse, NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-import { AnyZodObject } from 'zod/lib';
+// import { AnyZodObject } from 'zod/lib';
 
 export default function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  // const PUBLIC_FILE = /\.(.*)$/;
+  // if (
+  //   pathname.startsWith('/_next') || // exclude Next.js internals
+  //   pathname.startsWith('/api') || //  exclude all API routes
+  //   pathname.startsWith('/static') || // exclude static files
+  //   PUBLIC_FILE.test(pathname) // exclude all files in the public folder
+  // ) {
+  //   return NextResponse.next();
+  // }
+
   return NextResponse.next();
 }
 
-const validate =
-  (schema: AnyZodObject) => (req: NextRequest, res: NextResponse) => {
-    try {
-      schema.parse({
-        body: req.body,
-        params: req.nextUrl.searchParams,
-      });
-      return NextResponse.next();
-    } catch (error: any) {
-      return new NextResponse(
-        JSON.stringify({ success: false, message: error }),
-        { status: 40, headers: { 'content-type': 'application/json' } }
-      );
-    }
-  };
+// const validate =
+//   (schema: AnyZodObject) => (req: NextRequest, res: NextResponse) => {
+//     try {
+//       schema.parse({
+//         body: req.body,
+//         params: req.nextUrl.searchParams,
+//       });
+//       // return NextResponse.next();
+//     } catch (error: any) {
+//       return new NextResponse(
+//         JSON.stringify({ success: false, message: error }),
+//         { status: 40, headers: { 'content-type': 'application/json' } }
+//       );
+//     }
+//   };
+// export const config = {
+//   matcher: '/api/:path*',
+// };
