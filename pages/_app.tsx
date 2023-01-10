06bbc921d9658aaf2ddc5ec0890e3ed797f579b9
Stanley Tsonev <stanleytsonev@hotmail.com>
2023-01-10 09:46:22 -0500

    next fonts, import global font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Source_Sans_3 } from '@next/font/google';
+
+const sourceSans3 = Source_Sans_3({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={sourceSans3.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
