022b4bb618aec41d1610e1dbdfb5e3d2ac02a99a
Stanley Tsonev <stanleytsonev@hotmail.com>
2023-01-17 22:55:28 -0500

    ui changes

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,18 @@
 import { useRouter } from 'next/router';
+import styles from '@/styles/NavBar.module.css';
 
 const NavBar = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
   //
   return (
     <>
-      {router.pathname !== '/login' && <div>weed</div>}
+      {router.pathname !== '/login' && (
+        <div className={`${styles.navGrid} w-full`}>
+          <div className="col-start-1 col-end-3">1</div>
+          <div className="col-start-6 col-end-8">2</div>
+          <div className="col-start-11 col-end-13">3</div>
+        </div>
+      )}
 
       {children}
     </>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ const sourceSans3 = Source_Sans_3({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${sourceSans3.className} bg-blueGrey-500`}>
+    <main className={`${sourceSans3.className} bg-blueGrey-500 `}>
       <NavBar>
         <Component {...pageProps} />
       </NavBar>
