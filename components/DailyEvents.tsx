2e1bf1dda119a08a3ab63d169ab13a39ac96c385
Stanley Tsonev <stanleytsonev@hotmail.com>
2023-01-28 09:17:47 -0500

    daily events

--- components/DailyEvents.tsx
+++ components/DailyEvents.tsx
@@ -1,4 +1,31 @@
 export default function DailyEvents() {
-
-    return (<><div></div></>)
+  return (
+    <>
+      <div></div>
+    </>
+  );
 }
+
+const events = [
+  {
+    id: 4,
+    name: "John's Birthday",
+    type: 'BIRTHDAY',
+    description: '',
+    startDate: '2023/01/28',
+  },
+  {
+    id: 3,
+    name: 'Business meeting',
+    type: 'MEETING',
+    description: 'corporate meeting',
+    startDate: '2023/01/29',
+  },
+  {
+    id: 5,
+    name: 'Uncles wedding',
+    type: 'CELEBRATION',
+    description: '',
+    startDate: '2023/01/31',
+  },
+];
